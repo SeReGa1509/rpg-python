94364f32efa53aa289ad6eecf72cd6416428b8a6
Sergiy Mandrykin <sergey.mandrykin1509@gmail.com>
2018-05-10 19:08:53 +0300

    Player moving

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import pygame
 from Constants import *
+from Player import *
 
 class Main():
     def __init__(self, screen):
@@ -9,17 +10,47 @@ class Main():
         """
         self.screen = screen
         self.background = pygame.image.load('images/background.jpg')
+        self.player = Player("Sergiy")
         self.running = True
         self.main_loop()
 
     def handle_events(self):
-        pass
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                self.running = False
+
+            # Moving player
+            elif e.type == pygame.KEYDOWN: # On key down
+                if e.key == pygame.K_RIGHT:
+                    self.player.direction = RIGHT
+                    self.player.moving = [1, 0, 0, 0]
+                if e.key == pygame.K_DOWN:
+                    self.player.direction = DOWN
+                    self.player.moving = [0, 1, 0, 0]
+                if e.key == pygame.K_LEFT:
+                    self.player.direction = LEFT
+                    self.player.moving = [0, 0, 1, 0]
+                if e.key == pygame.K_UP:
+                    self.player.direction = UP
+                    self.player.moving = [0, 0, 0, 1]
+
+            elif e.type == pygame.KEYUP: # On key up
+                if e.key == pygame.K_UP:
+                    self.player.moving[UP] = 0
+                if e.key == pygame.K_DOWN:
+                    self.player.moving[DOWN] = 0
+                if e.key == pygame.K_LEFT:
+                    self.player.moving[LEFT] = 0
+                if e.key == pygame.K_RIGHT:
+                    self.player.moving[RIGHT] = 0
+
 
     def render(self):
         """
         Rendering everything
         """
         self.screen.blit(self.background, (0, 0))
+        self.player.render(screen)
         pygame.display.flip()
 
     def main_loop(self):
@@ -27,7 +58,9 @@ class Main():
         The main loop
         """
         while self.running == True:
+            self.player.move()
             self.render()
+            self.handle_events()
 
 
 pygame.init()
--- Player.py
+++ Player.py
@@ -2,13 +2,20 @@ import pygame
 from Constants import *
 
 class Player():
-    def __init__(self):
+    def __init__(self, name):
+        """
+        Initializing
+        :param name:
+        :rtype: object
+        """
         self.state = ALIVE
-        self.position = [START_X, START_Y, RIGHT]
-        self.name = "Sergiy"
+        self.direction = RIGHT
+        self.x = START_X
+        self.y = START_Y
+        self.name = name
         self.hp = MAX_HP
         self.mp = MAX_MP
-        self.image_pack = ["sprites/player.png"]
+        self.image_pack = ["sprites/player.png", "sprites/player.png", "sprites/player.png", "sprites/player.png"]
         self.images = []
 
         for image in self.image_pack:
@@ -20,13 +27,31 @@ class Player():
             i.append(temp.subsurface(264, 101, 75, 75))
             self.images.append(i)
 
-        self.moving[0, 0, 0, 0]
+        self.moving = [0, 0, 0, 0]
 
     def move(self):
-        pass
+        # Handle moving
+        if self.moving[RIGHT] == 1:
+            self.x += PLAYER_SPEED
+        if self.moving[DOWN] == 1:
+            self.y += PLAYER_SPEED
+        if self.moving[LEFT] == 1:
+            self.x -= PLAYER_SPEED
+        if self.moving[UP] == 1:
+            self.y -= PLAYER_SPEED
+
+        # Check out where player is
+        if self.x <= 0:
+            self.x = 0
+        if self.y <= 0:
+            self.y = 0
+        if self.x >= SCREEN_WIDTH - PLAYER_WIDTH:
+            self.x = SCREEN_WIDTH - PLAYER_WIDTH
+        if self.y >= SCREEN_HEIGHT - PLAYER_HEIGHT:
+            self.y = SCREEN_HEIGHT - PLAYER_HEIGHT
 
     def render(self, screen):
-        screen.blit(self.images[self.position[0]][self.state], (self.position[X], self.position[Y]))
+        screen.blit(self.images[self.direction][self.state], (self.x, self.y))
 
     def render_ui(self, screen):
         pass
